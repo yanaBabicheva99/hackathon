65f31ef4303571fb77df5440a8db8c2bb2fb5ee2
DULCETq <maks.lebedev0404@mail.ru>
2022-11-25 22:02:42 +0300

    personalpage

--- client/src/component/PersonalCabinet/personal.tsx
+++ client/src/component/PersonalCabinet/personal.tsx
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './personal.css';
-import { Breadcrumb, Layout, Menu, Card, Avatar } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { Layout, Input, Card, Avatar, Button, } from 'antd';
+import { UserOutlined, FileAddOutlined } from '@ant-design/icons';
+import type { SizeType } from 'antd/es/config-provider/SizeContext';
+
 
 const { Header, Content, Footer } = Layout;
+const { TextArea } = Input;
 
-const Personal: React.FC = () => (
-  <Layout>
-    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 32 }}>
-      <div className="site-layout-background" style={{ padding: 0, minHeight: 380 }}>
-      <Card title="Пользователь" extra={<a href="#">Изменить</a>}> 
-      <div className="card-redact">
-      <div>
-      <Avatar size={96} icon={<UserOutlined />} />
-    </div> 
-    <Card type="inner" title="Фамилия Имя Отчество" style={{width: '100%', marginLeft: '15px'}} >
-      Попов Жопа Очкович
-    </Card>   
-    </div>
-  </Card>
-      </div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}></Footer>
-  </Layout>
-);
+
+const Personal: React.FC = () => {
+  const [size, setSize] = useState<SizeType>('large');
+  return (
+    <Layout>
+      <Content className="site-layout" style={{ padding: '0 50px', marginTop: 32 }}>
+        <div className="site-layout-background" style={{ padding: 0, minHeight: 380 }}>
+          <Card title="Пользователь" extra={<a href="#">Изменить</a>}>
+            <div className="card-redact">
+              <div>
+                <Avatar size={96} icon={<UserOutlined />} />
+              </div>
+              <Card type="inner" title="Фамилия Имя Отчество" style={{ width: '100%', marginLeft: '15px' }} >
+                Попов Жопа Очкович
+              </Card>
+              </div>
+              <Card type="inner" title="Заметки" style={{ marginLeft: '111px', marginTop: '15px' }} >
+                <TextArea rows={4} />
+                <br />
+              </Card>  
+          </Card>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        <Button type="primary" icon={<FileAddOutlined />} size={size} >
+          Создать тест
+        </Button>
+      </Footer>
+    </Layout>
+  );
+}
 
 export default Personal;
